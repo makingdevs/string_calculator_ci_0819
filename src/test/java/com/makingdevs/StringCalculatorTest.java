0a3be1778091168b22be4918296b502ee2f003e5
neodevelop <juan@makingdevs.com>
2019-06-08 09:54:25 -0500

    Setup de pruebas para String Calculator

---
+++ src/test/java/com/makingdevs/StringCalculatorTest.java
@@ -0,0 +1,13 @@
+package com.makingdevs;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class StringCalculatorTest {
+
+  @Test
+  public void testSome(){
+    assertEquals("uno", "dos");
+  }
+
+}
