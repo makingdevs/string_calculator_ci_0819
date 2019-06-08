a2ae97c094b3bece90695d7504887257ff74f43d
neodevelop <juan@makingdevs.com>
2019-06-08 10:06:46 -0500

    Primer prueba superada

--- src/test/java/com/makingdevs/StringCalculatorTest.java
+++ src/test/java/com/makingdevs/StringCalculatorTest.java
@@ -6,8 +6,11 @@ import static org.junit.Assert.*;
 public class StringCalculatorTest {
 
   @Test
-  public void testSome(){
-    assertEquals("uno", "dos");
+  public void testEmpty(){
+    StringCalculator sc = new StringCalculator();
+    int result = sc.add("");
+    assertEquals(0, result);
   }
 
+  
 }
