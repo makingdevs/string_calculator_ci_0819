a2629fea43a3a4736c307fedd9c9af2032e357d0
GabrielMorenoGarrido <gabomgar@gmail.com>
2019-06-09 17:53:49 -0500

    prueba superada sobrecarga de metodo computer Lista de enteros retorno de String Fizz Buzz

--- src/main/java/com/makingdevs/FizzBuzzGabrielMGA.java
+++ src/main/java/com/makingdevs/FizzBuzzGabrielMGA.java
@@ -1,5 +1,8 @@
 package com.makingdevs;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FizzBuzzGabrielMGA {
 
     public String computer(int number) {
@@ -15,4 +18,25 @@ public class FizzBuzzGabrielMGA {
 
         return "";
     }
+
+    public List<String> computer(List<Integer> numbers) {
+
+        List<String> resultNumbersList = new ArrayList<>();
+
+        if (!(numbers == null || numbers.isEmpty())) {
+            for(int number : numbers) {
+                if(number % 3 == 0 && number % 5 == 0){
+                    resultNumbersList.add("FizzBuzz");
+                } else if (number % 3 == 0) {
+                    resultNumbersList.add("Fizz");
+                } else if (number % 5 == 0) {
+                    resultNumbersList.add("Buzz");
+                } else
+                    resultNumbersList.add(Integer.toString(number));
+            }
+        }
+
+        return resultNumbersList;
+    }
+
 }
--- src/test/java/com/makingdevs/FizzBuzzGabrielMGATest.java
+++ src/test/java/com/makingdevs/FizzBuzzGabrielMGATest.java
@@ -2,6 +2,10 @@ package com.makingdevs;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 public class FizzBuzzGabrielMGATest {
@@ -25,5 +29,33 @@ public class FizzBuzzGabrielMGATest {
         FizzBuzzGabrielMGA fzg = new FizzBuzzGabrielMGA();
         String result = fzg.computer(30);
         assertEquals("FizzBuzz",result);
+    }
+
+    @Test
+    public void overloadDivisibleNumbers() {
+        FizzBuzzGabrielMGA fzg = new FizzBuzzGabrielMGA();
+        List<String> arrayEsperado = new ArrayList<>();
+        arrayEsperado.add("1");
+        arrayEsperado.add("2");
+        arrayEsperado.add("Fizz");
+        arrayEsperado.add("4");
+        arrayEsperado.add("Buzz");
+        arrayEsperado.add("Fizz");
+        arrayEsperado.add("7");
+        arrayEsperado.add("8");
+        arrayEsperado.add("Fizz");
+        arrayEsperado.add("Buzz");
+        arrayEsperado.add("11");
+        arrayEsperado.add("Fizz");
+        arrayEsperado.add("13");
+        arrayEsperado.add("14");
+        arrayEsperado.add("FizzBuzz");
+
+        List<Integer> numbers = new ArrayList<>();
+        for(int i = 1; i<=15; i++){
+            numbers.add(i);
+        }
+        List<String> result = fzg.computer(numbers);
+        assertEquals(arrayEsperado,result);
     }
 }
