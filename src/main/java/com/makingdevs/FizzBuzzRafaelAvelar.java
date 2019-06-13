4baefe537afe40792affa05b5bc23d86219e8fe7
Rafael Avelar <rafaelksd@live.com>
2019-06-12 19:15:48 -0500

    Prueba exitosa, todos los numeros divisibles entre 3 y5 regresan la palabra FizzBuzz. RAL

--- src/main/java/com/makingdevs/FizzBuzzRafaelAvelar.java
+++ src/main/java/com/makingdevs/FizzBuzzRafaelAvelar.java
@@ -2,12 +2,18 @@ package com.makingdevs;
 
 public class FizzBuzzRafaelAvelar {
 
+    private static final String FIZZ_WORD = "Fizz";
+    private static final String BUZZ_WORD = "Buzz";
+    private static final String FIZZBUZZ_WORD = "FizBuzz";
+
     public String compute(int entero){
 
-        if(entero % 3 == 0){
-            return "Fizz";
+        if(entero % 3 == 0 && entero % 5 == 0){
+            return FIZZBUZZ_WORD;
+        }else if(entero % 3 == 0){
+            return FIZZ_WORD;
         }else if(entero % 5 == 0){
-            return "Buzz";
+            return BUZZ_WORD;
         }else{
             return String.valueOf(entero);
         }
--- src/test/java/com/makingdevs/FizzBuzzRafaelAvelarTest.java
+++ src/test/java/com/makingdevs/FizzBuzzRafaelAvelarTest.java
@@ -7,7 +7,8 @@ import org.junit.Test;
 public class FizzBuzzRafaelAvelarTest {
 
     private static final String FIZZ_WORD = "Fizz";
-    private static final String FIZZBUZZ_WORD = "Buzz";
+    private static final String BUZZ_WORD = "Buzz";
+    private static final String FIZZBUZZ_WORD = "FizBuzz";
 
     @Test
     public void divideEntre3RegresaFizzTest(){
@@ -28,18 +29,41 @@ public class FizzBuzzRafaelAvelarTest {
     public void divideEntre5RegresaBuzzTest(){
         FizzBuzzRafaelAvelar fizzRaff = new FizzBuzzRafaelAvelar();
         String resultado = fizzRaff.compute(5);
-        assertEquals(FIZZBUZZ_WORD, resultado);
+        assertEquals(BUZZ_WORD, resultado);
         assertEquals("1", fizzRaff.compute(1));
         assertEquals("2", fizzRaff.compute(2));
         assertEquals(FIZZ_WORD, fizzRaff.compute(3));
         assertEquals("4", fizzRaff.compute(4));
-        assertEquals(FIZZBUZZ_WORD, fizzRaff.compute(5));
+        assertEquals(BUZZ_WORD, fizzRaff.compute(5));
         assertEquals(FIZZ_WORD, fizzRaff.compute(6));
         assertEquals("7", fizzRaff.compute(7));
         assertEquals("8", fizzRaff.compute(8));
         assertEquals(FIZZ_WORD, fizzRaff.compute(9));
-        assertEquals(FIZZBUZZ_WORD, fizzRaff.compute(10));
+        assertEquals(BUZZ_WORD, fizzRaff.compute(10));
         //assertEquals(FIZZBUZZ_WORD, fizzRaff.compute(15));
-        assertEquals(FIZZBUZZ_WORD, fizzRaff.compute(20));
+        assertEquals(BUZZ_WORD, fizzRaff.compute(20));
+    }
+
+    @Test
+    public void divideEntre3y5RegresaFizzBuzzTest(){
+        FizzBuzzRafaelAvelar fizzRaff = new FizzBuzzRafaelAvelar();
+        String resultado = fizzRaff.compute(5);
+        assertEquals(BUZZ_WORD, resultado);
+        assertEquals("1", fizzRaff.compute(1));
+        assertEquals("2", fizzRaff.compute(2));
+        assertEquals(FIZZ_WORD, fizzRaff.compute(3));
+        assertEquals("4", fizzRaff.compute(4));
+        assertEquals(BUZZ_WORD, fizzRaff.compute(5));
+        assertEquals(FIZZ_WORD, fizzRaff.compute(6));
+        assertEquals("7", fizzRaff.compute(7));
+        assertEquals("8", fizzRaff.compute(8));
+        assertEquals(FIZZ_WORD, fizzRaff.compute(9));
+        assertEquals(BUZZ_WORD, fizzRaff.compute(10));
+        assertEquals(FIZZBUZZ_WORD, fizzRaff.compute(15));
+        assertEquals(BUZZ_WORD, fizzRaff.compute(20));
+        assertEquals(FIZZBUZZ_WORD, fizzRaff.compute(30));
+        assertEquals(BUZZ_WORD, fizzRaff.compute(40));
+        assertEquals(BUZZ_WORD, fizzRaff.compute(50));
+        assertEquals(FIZZBUZZ_WORD, fizzRaff.compute(60));
     }
 }
