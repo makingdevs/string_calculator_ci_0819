27cb6e34a12ad5e87fd332875542b43c693306d9
Rafael Avelar <rafaelksd@live.com>
2019-06-12 19:05:27 -0500

    Todos los numeros divisibles entre 5 regresan Buzz. RAL

--- src/main/java/com/makingdevs/FizzBuzzRafaelAvelar.java
+++ src/main/java/com/makingdevs/FizzBuzzRafaelAvelar.java
@@ -4,8 +4,10 @@ public class FizzBuzzRafaelAvelar {
 
     public String compute(int entero){
 
-        if(entero%3 == 0){
+        if(entero % 3 == 0){
             return "Fizz";
+        }else if(entero % 5 == 0){
+            return "Buzz";
         }else{
             return String.valueOf(entero);
         }
--- src/test/java/com/makingdevs/FizzBuzzRafaelAvelarTest.java
+++ src/test/java/com/makingdevs/FizzBuzzRafaelAvelarTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 public class FizzBuzzRafaelAvelarTest {
 
     private static final String FIZZ_WORD = "Fizz";
-    private static final String FIZZBUZZ_WORD = "FizzBuzz";
+    private static final String FIZZBUZZ_WORD = "Buzz";
 
     @Test
     public void divideEntre3RegresaFizzTest(){
@@ -18,10 +18,28 @@ public class FizzBuzzRafaelAvelarTest {
         assertEquals("2", fizzRaff.compute(2));
         assertEquals(FIZZ_WORD, fizzRaff.compute(3));
         assertEquals("4", fizzRaff.compute(4));
-        assertEquals("5", fizzRaff.compute(5));
         assertEquals(FIZZ_WORD, fizzRaff.compute(6));
         assertEquals("7", fizzRaff.compute(7));
         assertEquals("8", fizzRaff.compute(8));
         assertEquals(FIZZ_WORD, fizzRaff.compute(9));
+    }
+
+    @Test
+    public void divideEntre5RegresaBuzzTest(){
+        FizzBuzzRafaelAvelar fizzRaff = new FizzBuzzRafaelAvelar();
+        String resultado = fizzRaff.compute(5);
+        assertEquals(FIZZBUZZ_WORD, resultado);
+        assertEquals("1", fizzRaff.compute(1));
+        assertEquals("2", fizzRaff.compute(2));
+        assertEquals(FIZZ_WORD, fizzRaff.compute(3));
+        assertEquals("4", fizzRaff.compute(4));
+        assertEquals(FIZZBUZZ_WORD, fizzRaff.compute(5));
+        assertEquals(FIZZ_WORD, fizzRaff.compute(6));
+        assertEquals("7", fizzRaff.compute(7));
+        assertEquals("8", fizzRaff.compute(8));
+        assertEquals(FIZZ_WORD, fizzRaff.compute(9));
+        assertEquals(FIZZBUZZ_WORD, fizzRaff.compute(10));
+        //assertEquals(FIZZBUZZ_WORD, fizzRaff.compute(15));
+        assertEquals(FIZZBUZZ_WORD, fizzRaff.compute(20));
     }
 }
