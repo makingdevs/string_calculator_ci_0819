7aff6043778dcb26b8566976e5ce182213cebca9
Rolando Ardelio cruz juarez <rcruzj@axtel.com.mx>
2019-06-13 11:24:14 -0500

    creando clase FizzBuzzRoland

---
+++ src/main/java/com/makingdevs/FizzBuzzRoland.java
@@ -0,0 +1,15 @@
+package com.makingdevs;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FizzBuzzRoland {
+
+    public String compute(int number) {
+        return "";
+    }
+
+    public List<String> compute(List<Integer> numbersList) {
+        return null;
+    }
+}
