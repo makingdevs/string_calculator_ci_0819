335452323180c6441ac601530d041cdc1fd828e3
Rafael Avelar <rafaelksd@live.com>
2019-06-12 22:52:26 -0500

    Se quitaron los ifs anidados. RAL

--- src/main/java/com/makingdevs/FizzBuzzRafaelAvelar.java
+++ src/main/java/com/makingdevs/FizzBuzzRafaelAvelar.java
@@ -22,15 +22,13 @@ public class FizzBuzzRafaelAvelar {
         return respuestaList;
     }
 
+
     private String obtienePalabra(int entero) {
-        if (entero % 3 == 0 && entero % 5 == 0) {
-            return Constants.FIZZBUZZ_WORD;
-        } else if (entero % 3 == 0) {
-            return Constants.FIZZ_WORD;
-        } else if (entero % 5 == 0) {
-            return Constants.BUZZ_WORD;
-        } else {
-            return String.valueOf(entero);
-        }
+
+        if (entero % 3 == 0 && entero % 5 == 0) return Constants.FIZZBUZZ_WORD;
+        if (entero % 3 == 0) return Constants.FIZZ_WORD;
+        if (entero % 5 == 0) return Constants.BUZZ_WORD;
+
+        return String.valueOf(entero);
     }
 }
